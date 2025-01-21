05281d212c3920e860ed2756582874d8ac5b06fb
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-01-21 12:58:20 -0400

    Add Clientes class

---
+++ Models/Clientes.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistrosTecnico.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+
+    public DateTime FechaIngreso { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "El Nombres obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten Numero")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "La direccion es obligatoria")]
+    public string? Direccion { get; set; }
+
+    [Required(ErrorMessage = "El RNC es obligatorio.")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "El RNC debe contener 10 dígitos.")]
+    public string RNC { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El límite de crédito es obligatorio.")]
+    [Range(0, 1000000, ErrorMessage = "El límite de crédito debe estar entre 0 y 1,000,000.")]
+    public decimal LimiteCredito { get; set; }
+
+    [ForeignKey("TecnicoId")]
+    public virtual Tecnicos Tecnicos { get; set; } = null!;
+}
